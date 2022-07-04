93712563b5ff88286afc72098404adc88d4dcd4a
MadanMohanReddy <madan.patakota@hotmail.com>
2022-07-04 08:15:07 +0530

    US changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularSeivcesMay';
+export class AppComponent implements OnInit {
+  constructor(private httpclient:HttpClient){
+  }
+  jsonPlaceArray:any = [];
+  users:any = [];
+  ngOnInit(): void {
+  //   this.httpclient.get("https://jsonplaceholder.typicode.com/posts").subscribe((x)=>{
+  //   this.jsonPlaceArray = x;
+  // })
+
+    this.httpclient.get("http://localhost:3000/users").subscribe((x)=>{
+    //this.jsonPlaceArray = x;
+    console.log(x);
+    this.users = x;
+  })
+
+
+  }
 }
